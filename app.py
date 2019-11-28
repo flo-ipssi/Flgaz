cd1db8faf0eeec71d52422d464eb606032ba8d02
Florian Emmanuel <Flo>
2019-11-28 16:57:55 +0100

    fields & route

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 from flask import Flask, request, render_template, redirect, url_for
-import csv
-import sys
+import csv, setting
 
-
-# Après l'importation du module Flask 
-# On lui un nom d'application "app"
 app = Flask(__name__)
 
-# On définit une page (ou route) avec flask
-#@app.route permet de préciser à quelle adresse 
-#ce qui suit va s’appliquer.
+
 @app.route('/')
 def home():     
     return 'Bienvenue !'
 
-
-#Definition d'une route avec deux types de methode 
 @app.route('/gaz', methods=['GET','POST'])
 def save_gazouille():
 	if request.method == 'POST':
 		print(request.form)
 		dump_to_csv(request.form)
 		return redirect(url_for('timeline'))
-		#return "OK"
 	if request.method == 'GET':
 		return render_template('formulaire.html')
 
@@ -49,6 +40,22 @@ def parse_from_csv():
 
 def dump_to_csv(d):
 	donnees = [d["user-name"],d["user-text"] ]
-	with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
-		writer = csv.writer(f)
-		writer.writerow(donnees)
+	if len(d["user-text"]) <= 280 :
+		with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
+			writer = csv.writer(f)
+			writer.writerow(donnees)
+
+
+@app.route('/timeline/<username>', methods=['GET'])
+def timeline():
+	gaz = parse_user_from_csv(username)
+	return render_template("timeline.html", gaz = gaz)
+def parse_user_from_csv(user_id):
+    gaz = []
+	with open('./gazouilles.csv', 'r') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			if row[0] == user_id :
+				gaz.append({"user":row[0], "text":row[1]})
+	return gaz
+#d["user-text"][:280-]
---
+++ modules/models.py
@@ -0,0 +1,47 @@
+import de sqlalchemy
+from sqlalchemy import Column, Boolean, String, Integer, Numeric
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Declaration de la classe de déclaration des modèles de BDD
+Base = declarative_base() 
+
+# Création d'une classe selon la syntaxe objet SQLAlchemy
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column('username', String(255))
+    role = Column('role', String(255))
+    email = Column('email', String(255))
+    password = Column('password', String(255))
+    is_active = Column('is_active', Boolean)
+    is_connected = Column('is_connected', Boolean)
+    picture = Column('picture', Text)
+
+    # methode magique pour print()
+    def __str__(self):
+        return "{} | {}".format(self.username, self.email)
+
+class Tweet(Base):
+    __tablename__ = 'tweet'
+    id = Column(Integer, primary_key=True)
+    content = Column('content', String(64))
+    date = Column(Date, default=_get_date)
+    categorie = Column('categorie', Integer)
+    creator = Column('creator', Integer)
+
+    # methode magique pour print()
+    def __str__(self):
+        return "{} | {}".format(self.content, self.categorie)
+
+
+class Category(Base):
+    __tablename__ = 'tweet'
+    id = Column(Integer, primary_key=True)
+    name = Column('categorie',  String(255))
+
+    # methode magique pour print()
+    def __str__(self):
+        return "{}".format( self.name )
